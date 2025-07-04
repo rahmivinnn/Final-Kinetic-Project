cbc6796bade772509d58206135b090c2a26e2ea8
rahmivinnn <rahmivina230@gmail.com>
2025-07-04 10:18:04 +0700

    Update Next.js config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/Final-Kinetic-Project' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Final-Kinetic-Project/' : '/',
   eslint: {
     ignoreDuringBuilds: true,
   },
